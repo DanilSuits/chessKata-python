e6fb3037cf78a209fb50ae1be80dccff1ab158f4
Danil <danil@vast.com>
2018-03-11 23:37:13 -0400

    RED: Test Design

--- test/test_chessboard.py
+++ test/test_chessboard.py
@@ -2,12 +2,21 @@ import unittest
 
 from chesskata import board
 
-class MyTestCase(unittest.TestCase):
+class RandomBoardTestCase(unittest.TestCase):
     def test_random_board(self):
-        actual = board.random_pieces()
+        system_under_test = board
+        self.sut_satisifies_specification(system_under_test)
 
+    def sut_satisifies_specification(self, system_under_test):
+        pieces = system_under_test.random_pieces()
+        self.pieces_satisfy_specification(pieces)
+
+    def pieces_satisfy_specification(self, pieces):
+        self.pieces_are_truthy(pieces)
+
+    def pieces_are_truthy(self, pieces):
         """The order of the pieces on the home rank should be truthy"""
-        self.assertTrue(actual)
+        self.assertTrue(pieces, "Pieces should be truthy")
 
 
 if __name__ == '__main__':
