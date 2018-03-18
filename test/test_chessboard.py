ab0248f2548046d5ec3f8b6b1d23481b12dbe7d5
Danil <danil@vast.com>
2018-03-18 10:19:14 -0400

    GREEN: revert

--- test/test_chessboard.py
+++ test/test_chessboard.py
@@ -1,61 +1,15 @@
 import unittest
 
-class MockRandom:
-    def __init__(self, seed):
-        self.seed = seed
-
-    def randrange(self, *_):
-        return self.seed
-
 class RandomBoardTestCase(unittest.TestCase):
     def test_imperative_shell(self):
         from chesskata import shell as system_under_test
-        self.sut_satisfies_specification(system_under_test)
-
-    def test_core_random_pieces(self):
-        from chesskata import core as system_under_test
-        self.sut_satisfies_core_examples(system_under_test)
-
-    def sut_satisfies_core_examples(self, system_under_test):
-        self.example_matches(system_under_test, 0, "BBRKRQNN")
-        self.example_matches(system_under_test, 1, "BRBKRQNN")
-
-    def example_matches(self, system_under_test, seed, expected):
-        pieces = system_under_test.random_pieces(MockRandom(seed))
-        self.assertEqual(pieces, expected)
-
-    def test_core_exhausted_properties(self):
-        from chesskata import core as system_under_test
-        self.sut_satisfies_exhausted_properties(system_under_test)
-
-    def sut_satisfies_exhausted_properties(self, system_under_test):
-        variations = set()
-        for seed in range(0,960):
-            pieces = system_under_test.random_pieces(MockRandom(seed))
-            self.pieces_satisfy_specification(pieces)
-            variations.add(pieces)
-
-        self.assertEqual(len(variations), 960)
-
-    def test_core_with_samples(self):
-        from chesskata import core as system_under_test
-        self.sut_satisfies_samples(system_under_test)
-
-    def sut_satisfies_samples(self, system_under_test):
-        import random
-        variations = set()
-        samples_to_check = 2
-        for seed in random.sample(range(0,960), samples_to_check):
-            pieces = system_under_test.random_pieces(MockRandom(seed))
-            self.pieces_satisfy_specification(pieces)
-            variations.add(pieces)
-        self.assertEqual(len(variations), samples_to_check)
-
+        self.sut_satisifies_specification(system_under_test)
+        
     def test_random_board(self):
         from chesskata import board as system_under_test
-        self.sut_satisfies_specification(system_under_test)
+        self.sut_satisifies_specification(system_under_test)
 
-    def sut_satisfies_specification(self, system_under_test):
+    def sut_satisifies_specification(self, system_under_test):
         pieces = system_under_test.random_pieces()
         self.pieces_satisfy_specification(pieces)
 
