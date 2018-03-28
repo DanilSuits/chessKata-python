4267b18188cb1614088fbba035352177ecb61475
Danil <danil@vast.com>
2018-03-28 14:23:30 -0400

    GREEN: remove duplication

--- chesskata/core.py
+++ chesskata/core.py
@@ -4,10 +4,13 @@ def random_pieces(random):
 
 def fixed_pieces(index):
     if index < 2:
-        positions = [0,1,2] + [3,4,5,6,7]
-        if 1 == index:
-            positions = [2,1,0] + [3,4,5,6,7]
+        empty_squares = [3,4,5,6,7]
 
+        positions = [0,1,2]
+        if 1 == index:
+            positions = [2,1,0]
+
+        positions = positions + empty_squares
 
         pieceNames = "BBRKRQNN"
 
@@ -20,6 +23,4 @@ def fixed_pieces(index):
 
     else:
         return fixed_pieces(0)
-
-def positions2PieceNames(positions):
-    pass
+    
