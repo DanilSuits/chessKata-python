bcd50a29701dc827dc7d21716dfe0e8fa9f1a715
Danil <danil@vast.com>
2018-03-28 13:59:56 -0400

    GREEN: positions2PieceNames

--- chesskata/core.py
+++ chesskata/core.py
@@ -6,6 +6,17 @@ def fixed_pieces(index):
     if index < 2:
         if 1 == index:
             return "RBB" + "KRQNN"
-        return "BBR" + "KRQNN"
+        return positions2PieceNames([0,1,2] + [3,4,5,6,7])
     else:
         return fixed_pieces(0)
+
+def positions2PieceNames(positions):
+
+    pieceNames = "BBRKRQNN"
+
+    crntRow = ""
+    for p in range(8):
+        crntPiece = positions.index(p)
+        crntRow = crntRow + pieceNames[ crntPiece ]
+
+    return crntRow
