67da78b76007d68b64f85a0e7ade61285103316e
Danil <danil@vast.com>
2018-03-28 23:30:53 -0400

    RED test calibration

--- test/test_chessboard.py
+++ test/test_chessboard.py
@@ -15,7 +15,7 @@ class RandomBoardTestCase(unittest.TestCase):
     def sut_different_seeds_give_different_boards(self, system_under_test):
         import random
         samples = set()
-        for seed in random.sample(range(0,4), 2):
+        for seed in random.sample(range(0,5), 2):
             pieces = system_under_test.random_pieces(MockRandom(seed))
             samples.add(pieces)
 
@@ -27,10 +27,10 @@ class RandomBoardTestCase(unittest.TestCase):
 
     def sut_all_seeds_give_different_boards(self, system_under_test):
         samples = set()
-        for seed in range(0,4):
+        for seed in range(0,5):
             pieces = system_under_test.random_pieces(MockRandom(seed))
             samples.add(pieces)
-        self.assertEqual(len(samples), 4)
+        self.assertEqual(len(samples), 5)
 
     def test_core_with_examples(self):
         from chesskata import core as system_under_test
@@ -41,6 +41,7 @@ class RandomBoardTestCase(unittest.TestCase):
         self.sut_matches_example(system_under_test, 1, "RBBKRQNN")
         self.sut_matches_example(system_under_test, 2, "RBKRBQNN")
         self.sut_matches_example(system_under_test, 3, "RBKRQNBN")
+        self.sut_matches_example(system_under_test, 4, "BRKBRQNN")
 
     def sut_matches_example(self, system_under_test, seed, expected_pieces):
         pieces = system_under_test.random_pieces(MockRandom(seed))
