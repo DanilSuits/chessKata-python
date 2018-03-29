44f75fe1a56f31f1686fdb798b037dde269466f5
Danil <danil@vast.com>
2018-03-29 19:31:17 -0400

    GREEN: remove scaffolding

--- chesskata/core.py
+++ chesskata/core.py
@@ -1,50 +1,47 @@
 def random_pieces(random):
-    index = random.randrange(0,960)
+    index = random.randrange(0, 960)
     return fixed_pieces(index)
 
+
 def fixed_pieces(index):
-    if index < 960:
-        empty_squares = list(range(8))
+    empty_squares = list(range(8))
 
-        hints = []
-        for p in [4, 4, 20, 3]:
-            hints = hints + [ index % p ]
-            index = index // p
+    hints = []
+    for p in [4, 4, 20, 3]:
+        hints = hints + [index % p]
+        index = index // p
 
-        first_bishop_hint = hints[0]
-        second_bishop_hint = hints[1]
+    first_bishop_hint = hints[0]
+    positions = [2 * first_bishop_hint]
 
-        positions = [2 * first_bishop_hint, 2 * second_bishop_hint + 1]
+    empty_squares.remove(positions[0])
 
-        for p in positions:
-            empty_squares.remove(p)
+    second_bishop_hint = hints[1]
+    positions = positions + [2 * second_bishop_hint + 1]
 
-        # http://bridge.thomasoandrews.com/impossible/algorithm.html
-        import itertools
-        rkr = list(itertools.combinations(range(6),3))[hints[2]]
+    empty_squares.remove(positions[1])
 
-        for p in rkr:
-            positions = positions + [empty_squares[p]]
+    # http://bridge.thomasoandrews.com/impossible/algorithm.html
+    import itertools
+    rkr = list(itertools.combinations(range(6), 3))[hints[2]]
 
-        empty_squares.remove(positions[2])
-        empty_squares.remove(positions[3])
-        empty_squares.remove(positions[4])
+    for p in rkr:
+        positions = positions + [empty_squares[p]]
 
+    empty_squares.remove(positions[2])
+    empty_squares.remove(positions[3])
+    empty_squares.remove(positions[4])
 
-        positions = positions + [empty_squares[hints[3]]]
-        empty_squares.remove(positions[5])
-        
-        positions = positions + empty_squares
+    positions = positions + [empty_squares[hints[3]]]
+    empty_squares.remove(positions[5])
 
-        pieceNames = "BBRKRQNN"
+    positions = positions + empty_squares
 
-        crntRow = ""
-        for p in range(8):
-            crntPiece = positions.index(p)
-            crntRow = crntRow + pieceNames[ crntPiece ]
+    piece_names = "BBRKRQNN"
 
-        return crntRow
+    crnt_row = ""
+    for p in range(8):
+        crnt_piece = positions.index(p)
+        crnt_row = crnt_row + piece_names[crnt_piece]
 
-    else:
-        return fixed_pieces(0)
-    
+    return crnt_row
